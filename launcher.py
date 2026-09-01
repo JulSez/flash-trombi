e3bb8d30fd97b8287a63d48ac6ae2ab18f379329
JulSez <julien.seznec@proton.me>
2026-09-01 18:16:35 +0200

    Bundle shortlist review support

--- launcher.py
+++ launcher.py
@@ -11,6 +11,7 @@ import paths  # noqa: F401
 import pdf_import  # noqa: F401
 import practice_mode  # noqa: F401
 import progress_view  # noqa: F401
+import shortlist_review  # noqa: F401
 import storage  # noqa: F401
 import updates  # noqa: F401
 import version  # noqa: F401
